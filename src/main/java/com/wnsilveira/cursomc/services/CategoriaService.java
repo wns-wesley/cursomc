ff6985e1ce125faa5098a03f5d86767557bc6cd3
wnsilveira <wnsilveira@cambe.pr.gov.br>
2018-05-11 10:17:12 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/wnsilveira/cursomc/services/CategoriaService.java
+++ src/main/java/com/wnsilveira/cursomc/services/CategoriaService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.wnsilveira.cursomc.domain.Categoria;
 import com.wnsilveira.cursomc.repositories.CategoriaRepository;
+import com.wnsilveira.cursomc.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -16,7 +18,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
