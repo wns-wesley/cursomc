0487a767ed4fd9f4901a83a2cd0046bcde13ddd8
Wesley <wns.wesley@gmail.com>
2018-05-12 14:29:55 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/wnsilveira/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.wnsilveira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wnsilveira.cursomc.domain.Cliente;
+import com.wnsilveira.cursomc.repositories.ClienteRepository;
+import com.wnsilveira.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository categoriaRepository;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = categoriaRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
