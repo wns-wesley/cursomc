e02ff4cd01b1901c9b2bbd851c1551123339b5a9
Wesley <wns.wesley@gmail.com>
2018-05-12 15:28:08 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/wnsilveira/cursomc/CursomcApplication.java
+++ src/main/java/com/wnsilveira/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.wnsilveira.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.wnsilveira.cursomc.domain.Cidade;
 import com.wnsilveira.cursomc.domain.Cliente;
 import com.wnsilveira.cursomc.domain.Endereco;
 import com.wnsilveira.cursomc.domain.Estado;
+import com.wnsilveira.cursomc.domain.Pagamento;
+import com.wnsilveira.cursomc.domain.PagamentoComBoleto;
+import com.wnsilveira.cursomc.domain.PagamentoComCartao;
+import com.wnsilveira.cursomc.domain.Pedido;
 import com.wnsilveira.cursomc.domain.Produto;
+import com.wnsilveira.cursomc.domain.enums.EstadoPagamento;
 import com.wnsilveira.cursomc.domain.enums.TipoCliente;
 import com.wnsilveira.cursomc.repositories.CategoriaRepository;
 import com.wnsilveira.cursomc.repositories.CidadeRepository;
 import com.wnsilveira.cursomc.repositories.ClienteRepository;
 import com.wnsilveira.cursomc.repositories.EnderecoRepository;
 import com.wnsilveira.cursomc.repositories.EstadoRepository;
+import com.wnsilveira.cursomc.repositories.PagamentoRepository;
+import com.wnsilveira.cursomc.repositories.PedidoRepository;
 import com.wnsilveira.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -84,6 +96,23 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
 		
 	}
 }
---
+++ src/main/java/com/wnsilveira/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,27 @@
+package com.wnsilveira.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.wnsilveira.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {}
+
+	public PagamentoComCartao(EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+}
