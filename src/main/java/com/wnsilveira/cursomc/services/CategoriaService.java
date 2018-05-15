05ef1093e093445d3e2cc74657026aae4fadb4ad
Wesley <wns.wesley@gmail.com>
2018-05-15 20:43:42 -0300

    Validação sintatica com Bean validation

--- src/main/java/com/wnsilveira/cursomc/services/CategoriaService.java
+++ src/main/java/com/wnsilveira/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wnsilveira.cursomc.domain.Categoria;
+import com.wnsilveira.cursomc.dto.CategoriaDTO;
 import com.wnsilveira.cursomc.repositories.CategoriaRepository;
 import com.wnsilveira.cursomc.services.exceptions.DataIntegrityException;
 import com.wnsilveira.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
