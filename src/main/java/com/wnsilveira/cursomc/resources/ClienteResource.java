e6dca07cb88af1020249ec560683361f0440a2b7
Wesley <wns.wesley@gmail.com>
2018-05-14 21:54:28 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/wnsilveira/cursomc/resources/ClienteResource.java
+++ src/main/java/com/wnsilveira/cursomc/resources/ClienteResource.java
@@ -18,9 +18,9 @@ public class ClienteResource {
 	private ClienteService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> findById(@PathVariable Integer id) {
+	public ResponseEntity<Cliente> findById(@PathVariable Integer id) {
 		
-		Cliente obj = service.buscar(id);
+		Cliente obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
 	}
--- src/main/java/com/wnsilveira/cursomc/resources/PedidoResource.java
+++ src/main/java/com/wnsilveira/cursomc/resources/PedidoResource.java
@@ -18,9 +18,9 @@ public class PedidoResource {
 	private PedidoService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> findById(@PathVariable Integer id) {
+	public ResponseEntity<Pedido> findById(@PathVariable Integer id) {
 		
-		Pedido obj = service.buscar(id);
+		Pedido obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
 	}
