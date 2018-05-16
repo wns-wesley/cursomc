1ceae8660387d0248ca075921860f51868c19e66
wnsilveira <wnsilveira@cambe.pr.gov.br>
2018-05-16 16:41:46 -0300

    PUT, DELETE E GET para Cliente

--- src/main/java/com/wnsilveira/cursomc/domain/Cliente.java
+++ src/main/java/com/wnsilveira/cursomc/domain/Cliente.java
@@ -47,7 +47,12 @@ public class Cliente implements Serializable {
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo == null) ? null : tipo.getCod();
+	}
+	
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+		this(nome, email, cpfOuCnpj, tipo);
+		this.id = id;
 	}
 
 	public Integer getId() {
