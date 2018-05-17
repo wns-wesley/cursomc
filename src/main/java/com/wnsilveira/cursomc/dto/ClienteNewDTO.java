4e57d9346e3d0d09162cefec8d1fe5335091500d
wnsilveira <wnsilveira@cambe.pr.gov.br>
2018-05-17 10:48:38 -0300

    Validação customizada de CPF ou CNPJ na inserção de Cliente

--- src/main/java/com/wnsilveira/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/wnsilveira/cursomc/dto/ClienteNewDTO.java
@@ -2,22 +2,48 @@ package com.wnsilveira.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.wnsilveira.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Obrigatório")
+	@Email(message="Email inválido")
 	private String email;
+	
+	@NotEmpty(message="Obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message="Obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message="Obrigatório")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message="Obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
+	
 	private String telefone3;
 	
 	private Integer cidadeId;
